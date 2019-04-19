63630fc9e8a303e2601f015fa9dbc80c7e80aeef
Thord Setsaas <thordendre@gmail.com>
2019-04-19 12:46:05 +0200

    Added CORS Headers

--- controllers/controller_util.go
+++ controllers/controller_util.go
@@ -5,4 +5,7 @@ import "net/http"
 // SetHeaders will set the default headers used by all requests
 func SetHeaders(w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
