77b35b8bd8ba33f221879ed3971f03fc7bff9b02
Thord Setsaas <t.setsaas@sportradar.com>
2023-10-31 14:19:44 +0100

    Use tournament elo if available for odds

--- data/tournament.go
+++ data/tournament.go
@@ -307,7 +307,7 @@ func GetTournamentProbabilities(id int) ([]*models.Probability, error) {
 			m.id, m.created_at, m.updated_at, IF(TIMEDIFF(MAX(l.updated_at), NOW() - INTERVAL 15 MINUTE) > 0, 1, 0) AS 'is_started',
 			m.is_finished, m.is_abandoned, m.is_walkover, m.winner_id,
 			GROUP_CONCAT(DISTINCT p2l.player_id ORDER BY p2l.order) AS 'players',
-			GROUP_CONCAT(DISTINCT pe.current_elo ORDER BY p2l.order) AS 'elos',
+			GROUP_CONCAT(DISTINCT if(pe.tournament_elo_matches<6, pe.current_elo, pe.tournament_elo) ORDER BY p2l.order) AS 'elos',
 			(MAX(p.is_placeholder) - 1) * -1 AS 'is_players_decided',
 			mm.is_draw_possible
 		FROM matches m
